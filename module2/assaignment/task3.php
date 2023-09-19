f3a0873dde58db26cc0c3564faf1d7a0d38fb77c
Md. Chanchal Biswas <mchanchalbd@gmail.com>
2023-09-20 01:27:25 +0600

    Module 2

--- module2/assaignment/task3.php
+++ module2/assaignment/task3.php
@@ -4,10 +4,12 @@
 // Write a PHP program that calculates and prints the first 10 Fibonacci numbers. But, if a
 // Fibonacci number is greater than 100, break out of the loop using the break statement.
 
+$limit = 10; // Maximum print Fibonacci numbers
+
 $previous = 0;
 $current = 1;
 
-for ($i = 1; $i <= 10; $i++) {
+for ($i = 1; $i <= $limit; $i++) {
     if ($previous > 100) break; // Break out of loop
     echo $previous, " ";
 
